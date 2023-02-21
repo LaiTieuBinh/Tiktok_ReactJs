2b061b2d80ce5989ff9c3b5b427e59bfefee2129
binh <tieubinhlai@gmail.com>
2023-02-21 10:41:36 +0700

    Video 96: Dung khung layout mac dinh

--- src/Components/Layout/components/Header/index.js
+++ src/Components/Layout/components/Header/index.js
@@ -1,8 +1,16 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
 
+
+const cx = classNames.bind(styles);
 
 function Header() {
     return ( 
-        <h1>Header</h1>
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+
+            </div>
+        </header>
      );
 }
 
