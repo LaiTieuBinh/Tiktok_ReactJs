d3c0a2b6f5037848d5af0816009176957626a4a0
binh <tieubinhlai@gmail.com>
2023-03-17 16:39:16 +0700

    Video 110: Tai cau truc va toi uu code 1

---
+++ src/layouts/components/Search/index.js
@@ -0,0 +1,113 @@
+import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import HeadlessTippy from '@tippyjs/react/headless';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import classNames from 'classnames/bind';
+import { useEffect, useRef, useState } from 'react';
+
+import styles from './Search.module.scss';
+import { Wrapper as PopperWrapper } from '~/Components/Popper';
+import AccountItem from '~/Components/AccountItem';
+import { useDebounce } from '~/hooks';
+import * as searchServices from '~/services/searchService';
+
+const cx = classNames.bind(styles);
+
+function Search() {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchResult, setSearchResult] = useState([]);
+    const [showResult, setShowResult] = useState(true);
+    const [loading, setLoading] = useState(false);
+
+    const debounced = useDebounce(searchValue, 700);
+
+    const inputRef = useRef();
+
+    useEffect(() => {
+        if (!debounced.trim()) {
+            setSearchResult([]);
+
+            return;
+        }
+
+        const fetchApi = async () => {
+            setLoading(true);
+
+            const result = await searchServices.search(debounced);
+
+            setSearchResult(result);
+            setLoading(false);
+        };
+
+        fetchApi();
+    }, [debounced]);
+
+    const handleOnChangeInput = (e) => {
+        const searchValue = e.target.value;
+
+        if (searchValue.startsWith(' ')) {
+            if (searchValue != null) {
+                const position = parseInt(searchValue.length);
+                inputRef.current.setSelectionRange(position, position);
+            }
+            return;
+        } else {
+            setSearchValue(e.target.value);
+        }
+    };
+
+    const handleClear = () => {
+        setSearchValue('');
+        inputRef.current.focus();
+        setSearchResult([]);
+    };
+
+    const handleHideResult = () => {
+        setShowResult(false);
+    };
+
+    return (
+        
+        // <div>
+            <HeadlessTippy
+                interactive
+                appendTo={() => document.body}
+                visible={showResult && searchResult.length > 0}
+                onClickOutside={handleHideResult}
+                render={(attrs) => (
+                    <div className={cx('search-result')} tabIndex="-1" {...attrs}>
+                        <PopperWrapper>
+                            <h4 className={cx('search-title')}>Accounts</h4>
+                            {searchResult.map((result) => {
+                                return <AccountItem key={result.id} data={result} />;
+                            })}
+                        </PopperWrapper>
+                    </div>
+                )}
+            >
+                <div className={cx('search')}>
+                    <input
+                        value={searchValue}
+                        placeholder="Search accounts and videos"
+                        spellCheck={false}
+                        ref={inputRef}
+                        onChange={handleOnChangeInput}
+                        onFocus={() => setShowResult(true)}
+                    />
+    
+                    {!!searchValue && !loading && (
+                        <button className={cx('clear')} onClick={handleClear}>
+                            <FontAwesomeIcon icon={faCircleXmark} />
+                        </button>
+                    )}
+                    {loading && <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />}
+    
+                    <button className={cx('search-btn')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+            </HeadlessTippy>
+        // </div>
+    );
+}
+
+export default Search;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 // Layout
-import { HeaderOnly } from '~/Components/Layout';
-import routesConfig from '~/config/routes';
+import { HeaderOnly } from '~/layouts';
+import config from '~/config';
 
 // Pages
 import Home from '~/Pages/Home';
@@ -9,25 +9,15 @@ import Profile from '~/Pages/Profile';
 import Upload from '~/Pages/Upload';
 import Search from '~/Pages/Search';
 
-
-
-
-
 // Public routes
 const publicRoutes = [
-    { path: routesConfig.home, component: Home },
-    { path: routesConfig.following, component: Following },
-    { path: routesConfig.profile, component: Profile },
-    { path: routesConfig.upload, component: Upload, layout: HeaderOnly },
-    { path: routesConfig.search, component: Search, layout: null },
+    { path: config.routes.home, component: Home },
+    { path: config.routes.following, component: Following },
+    { path: config.routes.profile, component: Profile },
+    { path: config.routes.upload, component: Upload, layout: HeaderOnly },
+    { path: config.routes.search, component: Search, layout: null },
+];
 
+const privateRoutes = [];
 
-]
-
-
-const privateRoutes = [
-
-]
-    
-
-export { publicRoutes, privateRoutes }
+export { publicRoutes, privateRoutes };
